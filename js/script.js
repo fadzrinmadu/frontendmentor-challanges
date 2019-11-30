cb89b3dfdd3720f36a0cd8c4d5c3f850f94ea144
fadzrinmadu <aan.fadzrin@gmail.com>
2019-12-01 02:10:10 +0700

    firt commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  const form = document.querySelector('form');
+  const inputEmail = document.querySelector('#email');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const pattern = /^([\w\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+    if(!pattern.test(inputEmail.value)) {
+      // hapus pesan invalid jika ada
+      if (form.querySelector('span')) form.removeChild(form.querySelector('span'));
+
+      const span = document.createElement('span');
+      const spanText = document.createTextNode('Please provide a valid email');
+      span.appendChild(spanText);
+      form.appendChild(span);
+
+      inputEmail.classList.add('invalid');
+    }
+  })
+
+})
